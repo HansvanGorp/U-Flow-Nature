2d965cf7de276458be96909008b2bf42b74eff24
HansvanGorp <60602887+HansvanGorp@users.noreply.github.com>
2023-03-06 09:39:09 +0100

    Initial commit

---
+++ overnight_statistics.py
@@ -0,0 +1,197 @@
+# %% imports
+# standard libraries
+import torch
+from tqdm import tqdm
+
+# %% caclulate statistics functions
+def get_TST(predictions_subject):
+    _predictions_subject = predictions_subject.clone()
+    _predictions_subject[predictions_subject==0]=-1
+    TST = (_predictions_subject!=-1).sum(dim=1)/2
+    return TST
+
+def get_SOL(predictions_subject):
+    _predictions_subject = predictions_subject.clone()
+    _predictions_subject[predictions_subject==-1] = 0
+    
+    SOL = torch.argmax((_predictions_subject!=0)*1,dim=1)/2
+    return SOL
+
+def get_REMOL(predictions_subject):
+    REMOL = torch.argmax((predictions_subject==4)*1,dim=1)/2 - get_SOL(predictions_subject)
+    return REMOL
+
+def get_WASO(predictions_subject):
+    # total size
+    size = (predictions_subject.size(1)/2)
+    
+    # total sleep time
+    TST = get_TST(predictions_subject)
+    
+    # first wake
+    first_wake = get_SOL(predictions_subject)
+    
+    # last wake
+    _predictions_subject = predictions_subject.clone()
+    last_wake = get_SOL(_predictions_subject.flip(dims=(1,)))
+    
+    # WASO
+    WASO = size - TST - first_wake - last_wake
+    
+    return WASO
+
+def split_into_bouts(predictions_subject):    
+    bouts = []
+    
+    for i in range(predictions_subject.size(0)):
+        predictions_here = predictions_subject[i,:]
+        bouts_here = torch.tensor_split(predictions_here, torch.where(torch.diff(predictions_here) != 0)[0]+1)
+        bouts.append(bouts_here)
+    return bouts
+
+def get_awakenings(predictions_subject):
+    bouts = split_into_bouts(predictions_subject)
+    
+    RA = torch.zeros(len(bouts))
+    NA = torch.zeros(len(bouts))
+    for i,bout in enumerate(bouts):
+        for j,b in enumerate(bout):
+            if j != 0:
+                if bout[j][0] == 0:
+                    if bout[j-1][0] == 4:
+                        RA[i] += 1
+                    else:
+                        NA[i] += 1
+    return RA, NA
+    
+
+def get_TIS(predictions_subject):
+    TIS = torch.zeros(predictions_subject.size(0),5)
+    for i in range(5):
+        TIS[:,i] = (predictions_subject==i).sum(dim=1)/2
+    return TIS
+
+def get_all_overnight_stats(predictions):
+    no_subjects     = predictions.shape[0]
+    overnight_statistics = [[None for i in range(10)] for i in range(no_subjects)]
+    
+    # loop over subjects
+    for subject_id in tqdm(range(no_subjects)):
+        predictions_subject = predictions[subject_id,:,:]
+        
+        #check if predictions is empty (missing one scorer in hold-out test set)
+        if torch.any(predictions_subject[:,0]==-1): # if any start of with -1 do a thorough check
+            exist = torch.any(predictions_subject!=-1,dim=1)
+            predictions_subject = predictions_subject[exist,:]
+        
+        # calculate stats
+        TST   = get_TST(predictions_subject)       # Total Sleep Time
+        SOL   = get_SOL(predictions_subject)       # Sleep Onset Latency
+        REMOL = get_REMOL(predictions_subject)     # REM Onset Latency
+        WASO  = get_WASO(predictions_subject)      # Wake After Sleep Onset
+        RA,NA = get_awakenings(predictions_subject)# REM awakenings and NREM awakenings
+        TIS   = get_TIS(predictions_subject)       # Time in Stage
+        
+        # save to list
+        overnight_statistics[subject_id][0] = TST
+        overnight_statistics[subject_id][1] = SOL
+        overnight_statistics[subject_id][2] = REMOL
+        overnight_statistics[subject_id][3] = WASO
+        overnight_statistics[subject_id][4] = RA
+        overnight_statistics[subject_id][5] = NA
+        overnight_statistics[subject_id][6] = TIS[:,1]
+        overnight_statistics[subject_id][7] = TIS[:,2]
+        overnight_statistics[subject_id][8] = TIS[:,3]
+        overnight_statistics[subject_id][9] = TIS[:,4]
+        
+    return overnight_statistics
+
+def get_overnight_stats_one_subject(predictions, subject_id):
+    overnight_statistics = [None for i in range(10)]
+
+    predictions_subject = predictions[subject_id,:,:]
+    
+    #check if predictions is empty (missing one scorer in hold-out test set)
+    if torch.any(predictions_subject[:,0]==-1): # if any start of with -1 do a thorough check
+        exist = torch.any(predictions_subject!=-1,dim=1)
+        predictions_subject = predictions_subject[exist,:]
+    
+    # calculate stats
+    TST   = get_TST(predictions_subject)       # Total Sleep Time
+    SOL   = get_SOL(predictions_subject)       # Sleep Onset Latency
+    REMOL = get_REMOL(predictions_subject)     # REM Onset Latency
+    WASO  = get_WASO(predictions_subject)      # Wake After Sleep Onset
+    RA,NA = get_awakenings(predictions_subject)# REM awakenings and NREM awakenings
+    TIS   = get_TIS(predictions_subject)       # Time in Stage
+    
+    # save to list
+    overnight_statistics[0] = TST
+    overnight_statistics[1] = SOL
+    overnight_statistics[2] = REMOL
+    overnight_statistics[3] = WASO
+    overnight_statistics[4] = RA
+    overnight_statistics[5] = NA
+    overnight_statistics[6] = TIS[:,1]
+    overnight_statistics[7] = TIS[:,2]
+    overnight_statistics[8] = TIS[:,3]
+    overnight_statistics[9] = TIS[:,4]
+        
+    return overnight_statistics
+
+# %% caclutate metrics on those statistics
+def calculate_KL(mu1, sigma1, mu2, sigma2):
+    if sigma1 < 1e-2:
+        sigma1 = 1e-2
+            
+    if sigma2 < 1e-2:
+        sigma2 = 1e-2
+    
+    
+    KL_here = torch.log(torch.ones(1)*sigma2/sigma1) + (sigma1**2 + (mu1-mu2)**2)/(2*sigma2**2) - 1/2
+    return KL_here
+
+def convert_2_cdf(samples):
+    x_min = -208/2
+    x_max = 1792/2
+    no_bins = 2000
+    
+    hist = torch.histc(samples, bins = no_bins, min=x_min, max=x_max)
+    CDF = hist.cumsum(dim=0)
+    CDF = CDF/CDF[-1]
+    
+    return CDF
+
+def get_overnight_statistics_results(overnight_statistics_grth,overnight_statistics_methods):
+    no_methods = len(overnight_statistics_methods)
+    no_subjects = len(overnight_statistics_grth)
+    no_stats = 10
+    overnight_statistics_results = torch.zeros(3,no_stats,no_methods,no_subjects)
+
+    for stat_id in range(no_stats):
+        for method_id in range(no_methods):
+            for subject_id in range(no_subjects):
+                samples_here = overnight_statistics_methods[method_id][subject_id][stat_id]
+                samples_grth = overnight_statistics_grth[subject_id][stat_id]
+                
+                CDF_here = convert_2_cdf(samples_here)
+                CDF_grth = convert_2_cdf(samples_grth)
+                
+                diff = torch.abs(CDF_here-CDF_grth)
+                
+                # KL divergence
+                mu_here = samples_here.mean()
+                st_here = samples_here.std()
+                
+                mu_grth = samples_grth.mean()
+                st_grth = samples_grth.std()
+                
+                overnight_statistics_results[0,stat_id, method_id, subject_id] = calculate_KL(mu_grth,st_grth,mu_here,st_here)
+                
+                # Wasserstein metric
+                overnight_statistics_results[1,stat_id, method_id, subject_id] = diff.sum()*0.5
+                
+                # Kolmogorov-Smirnov metric
+                overnight_statistics_results[2,stat_id, method_id, subject_id] = diff.max()
+
+                
+    return overnight_statistics_results
---
+++ plot.py
@@ -0,0 +1,128 @@
+# %% imports
+# standard libraries
+import torch
+import argparse
+import matplotlib.pyplot as plt
+
+# local
+import majority_voting
+import plot_functions
+import overnight_statistics
+
+# %% loading
+# user inputs lists of names to compare against, default is the methods implemented in the manuscript
+parser = argparse.ArgumentParser(description='compare results')
+parser.add_argument('-names_to_compare',type=str,nargs="+",default=['U-Net_fact','U-Net_drop','U-Flow'])
+parser.add_argument('-subject_id',type=int,default=19)
+args = parser.parse_args()
+
+# load the predictions
+predictions_grth = torch.load("predictions/ground_truth.tar")
+predictions_methods = []
+no_methods = len(args.names_to_compare)
+for method_id in range(no_methods):
+    predictions_here = torch.load(f"predictions/{args.names_to_compare[method_id]}.tar")
+    predictions_methods.append(predictions_here)
+    
+# calculate majority votes
+majority_grth = majority_voting.get_majority_vote_human_panel(predictions_grth)
+majority_methods = []
+for method_id in range(no_methods):
+    majority_here = majority_voting.get_majority_vote(predictions_methods[method_id])
+    majority_methods.append(majority_here)
+
+
+# calculate overnight statistics
+overnight_statistics_grth = overnight_statistics.get_overnight_stats_one_subject(predictions_grth,args.subject_id)
+overnight_statistics_methods = []
+for method_id in range(no_methods):
+    overnight_statistics_here = overnight_statistics.get_overnight_stats_one_subject(predictions_methods[method_id],args.subject_id)
+    overnight_statistics_methods.append(overnight_statistics_here)
+    
+# %% append results into single lists
+predictions = predictions_methods + [predictions_grth]
+majority_votes = majority_methods + [majority_grth]
+overnight_statistics = overnight_statistics_methods + [overnight_statistics_grth]
+names = args.names_to_compare + ["ground truth"]
+
+# %% plot the hypnograms
+mask = (majority_grth[args.subject_id,:]!=-1)
+min_t = 0
+max_t = torch.ceil(((mask*1).argmin())/60)/2
+no_methods = len(names)
+plt.figure(figsize=(5*no_methods,7.5))
+
+for j,(prediction,majority_vote,name) in enumerate(zip(predictions,majority_votes,names)):        
+    for i in range(6):
+        plt.subplot(8,no_methods,j+i*no_methods+1)
+        plot_functions.plot_hypnogram(prediction[args.subject_id,i,:])
+        plt.ylabel('')
+        plt.grid()
+        
+        
+        if i == 0:
+            plt.title(f"hypnograms for {name}")
+        if i != 5:
+              plt.xticks([0,2,4,6,8],[])
+        
+        plt.xlim(min_t,max_t)
+        
+    # majority vote
+    plt.subplot(8,no_methods,j+7*no_methods+1)
+    plot_functions.plot_hypnogram(majority_vote[args.subject_id,:])
+    plt.ylabel('')
+    plt.xlim(min_t,max_t)
+    plt.grid()
+    plt.xlabel("hours of sleep")
+    plt.title(f"majority vote for {name}")
+    
+plt.savefig("figures//hypnograms.png",dpi=300,bbox_inches='tight')
+plt.close()
+
+# %% eCDF of overnight statistics
+stat_names = ['Total Sleep Time', 
+              'Sleep Onset Latency', 
+              'REM Latency', 
+              'WASO', 
+              'REM awakenings ',
+              'NREM awakenings',
+              'Time in N1',
+              'Time in N2',
+              'Time in N3',
+              'Time in REM']
+
+for i, stat_name in enumerate(stat_names):
+    plt.figure()
+    min_x = []
+    max_x = []
+    for j,stats in enumerate(overnight_statistics):  
+        samples = stats[i]
+        
+        min_x_here, max_x_here = plot_functions.plot_eCDF(samples)
+        
+        min_x.append(min_x_here)
+        max_x.append(max_x_here)
+        
+    # title
+    if j not in [4,5]:
+        plt.xlabel('time [min]')
+    else:
+        plt.xlabel('awakenings [-]')    
+    
+    plt.ylabel('cumulative probability')
+    
+    plt.title(stat_name)
+    
+    # min and max
+    x_min2 = min(min_x)*0.95
+    x_max2 = max(max_x)*1.05
+    
+    if x_min2 <= 5:
+        x_min2 = -1
+        
+    plt.xlim(x_min2,x_max2)
+    
+    plt.legend(names)
+    plt.grid()
+    plt.savefig(f"figures//{stat_name}.png",dpi=300,bbox_inches='tight')
+    plt.close()
